8fa7db2ed3a7c069add49f6884710bf965128a93
Yme-Jan Iedema <djnevs@gmail.com>
2017-05-01 20:31:32 +0200

    Development boolean in index.js to disable the load balancer and run locally

--- agent/index.js
+++ agent/index.js
@@ -1,8 +1,17 @@
+const dev = false;
+
 const webby = require('webby.js');
-const worker = require('../worker')();
 
-webby(worker.port, {
+let port = 8081;
+if (!dev) {
+    const worker = require('../worker')();
+    port         = worker.port;
+}
+
+webby(port, {
     controller_path: __dirname + '/controllers',
-    template_path: __dirname + '/theme',
+    template_path:   __dirname + '/theme',
+    middleware_path: __dirname + '/middleware',
+    static_path:     __dirname + '/static',
+    cache:           false,
 });
-
